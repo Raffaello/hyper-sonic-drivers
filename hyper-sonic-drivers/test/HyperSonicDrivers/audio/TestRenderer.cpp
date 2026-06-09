c1392540f2a2003c90c307b07344493c8f1d6209
Raffaello <raffaellobertini@gmail.com>
2026-06-09 13:50:56 +0100

    Unified Renderer (#300)
    * unified renderer
    
    * code rev
    
    * Fix ci
    
    * Fix SDL2 build
    
    * Remove mixer from renderer
    
    * code rev
    
    * code rev

--- hyper-sonic-drivers/test/HyperSonicDrivers/audio/TestRenderer.cpp
+++ hyper-sonic-drivers/test/HyperSonicDrivers/audio/TestRenderer.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include <HyperSonicDrivers/audio/sdl3/Renderer.hpp>
+#include <HyperSonicDrivers/audio/Renderer.hpp>
 #include <HyperSonicDrivers/devices/Adlib.hpp>
 #include <HyperSonicDrivers/devices/SbPro2.hpp>
 #include <HyperSonicDrivers/drivers/westwood/ADLDriver.hpp>
@@ -9,7 +9,7 @@
 #include <filesystem>
 #include <string>
 
-namespace HyperSonicDrivers::audio::sdl3
+namespace HyperSonicDrivers::audio
 {
 using audio::mixer::eChannelGroup;
 using hardware::opl::OplEmulator;
@@ -54,7 +54,7 @@ TEST_P(RendererTest, render_wav)
 
     ASSERT_FALSE(std::filesystem::exists(rfile));
     {
-        Renderer r(freq, 1024);
+        Renderer r(1024);
         r.openOutputFile(rfile);
 
         auto drv1 = drivers::westwood::ADLDriver(opl, eChannelGroup::Music);
@@ -95,7 +95,7 @@ TEST_P(RendererTest, render_wav2)
 
     ASSERT_FALSE(std::filesystem::exists(rfile));
     {
-        Renderer r(freq, 1024);
+        Renderer r(1024);
         r.openOutputFile(rfile);
 
         auto drv1 = drivers::westwood::ADLDriver(opl, eChannelGroup::Music);
@@ -128,7 +128,7 @@ INSTANTIATE_TEST_SUITE_P(
     ::testing::Values(
         std::make_tuple<>("adlib_mame2", 44100, eDeviceName::Adlib, OplEmulator::MAME),
         std::make_tuple<>("sbpro2_dosbox", 44100, eDeviceName::SbPro2, OplEmulator::DOS_BOX)));
-}    // namespace HyperSonicDrivers::audio::sdl3
+}    // namespace HyperSonicDrivers::audio
 
 int main(int argc, char** argv)
 {
