7fc4d56345ce6469fb6a673532a68272397b45fc
Raffaello <raffaellobertini@gmail.com>
2026-07-24 13:14:57 +0100

    Fix Loggers (#309)
    * Fix Loggers
    
    * code rev
    
    * Fix Sonarcloud
    
    * Fix Sonarcloud
    
    * code rev
    
    * code rev
    
    * Fix Sonarcloud
    
    * Fix Sonarcloud
    
    * Fix Sonarcloud
    
    * Fix Sonarcloud
    
    * Fix Sonarcloud
    
    * Fix Sonarcloud
    
    * Fix Sonarcloud
    
    * Fix Sonarcloud

--- hyper-sonic-drivers/src/HyperSonicDrivers/utils/sdl3/Logger.cpp
+++ hyper-sonic-drivers/src/HyperSonicDrivers/utils/sdl3/Logger.cpp
@@ -54,7 +54,7 @@ constexpr SDL_LogPriority level2sdl(const Logger::eLevel level)
 static SDL_LogOutputFunction default_log_output_function          = nullptr;
 static void*                 default_log_output_function_userdata = nullptr;
 
-[[maybe_unused]] static void log_output([[maybe_unused]] void* userdata, int category, SDL_LogPriority priority, const char* msg)
+[[maybe_unused]] static void SDLCALL log_output([[maybe_unused]] void* userdata, int category, SDL_LogPriority priority, const char* msg)
 {
     std::string p;
     std::string c;
@@ -129,11 +129,13 @@ Logger::Logger() : ILogger()
     SDL_GetLogOutputFunction(&default_log_output_function, &default_log_output_function_userdata);
     instance = this;
     setLevel(eLevel::Info);
+    enable();
 }
 
 Logger::~Logger()
 {
     fflush(nullptr);
+    disable();
 }
 
 void Logger::setLevelAll(const eLevel level)
@@ -180,12 +182,12 @@ void Logger::critical(const std::string& str, const eCategory cat)
 
 void Logger::enable()
 {
-    SDL_SetLogOutputFunction(default_log_output_function, default_log_output_function_userdata);
+    SDL_SetLogOutputFunction(&log_output, nullptr);
 }
 
 void Logger::disable()
 {
-    SDL_SetLogOutputFunction(nullptr, nullptr);
+    SDL_SetLogOutputFunction(default_log_output_function, default_log_output_function_userdata);
 }
 
 
